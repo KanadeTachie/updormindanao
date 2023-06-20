f20537733713a532eec1fc310df2df650d66ddfa
Powder <135595292+MediumCoffee@users.noreply.github.com>
2023-06-20 16:13:30 +0800

    Add files via upload

--- Main/html/index.php
+++ Main/html/index.php
@@ -11,74 +11,89 @@
     $result = $conn->query($sql);
 ?>
 
-
 <head>
     <meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Dormisko</title>
+    <title>UPDormindanao</title>
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto&display=swap" rel="stylesheet">
     
-    <link rel="stylesheet" href="../css/elements.css">
-    <link rel="stylesheet" href="../css/header.css">
-    <link rel="stylesheet" href="../css/navigation.css">
-    <link rel="stylesheet" href="../css/main.css">
     <script src="../js/jquery-1.12.3.min.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+    <link rel="stylesheet" href="scard.css">
+
+    <style>
+        nav {
+            background-image: url("UPMin_EBL.jpg");
+            background-size: cover;
+            background-attachment: fixed;
+            background-position-y: -650px;
+            background-repeat: no-repeat;
+        }
+
+        @media (max-width: 738px){
+            nav {
+                background-size: auto;
+                background-position-y: -1200px;
+            }
+        }
+    </style>
     
 </head>
 
 <body>
-    <header class="header">
-        <div class="left-section">
-            <a href="http://localhost/Dormisko/Main/html/">
-                <img class="logo" src="../images/Header/dormiskologo.png">
-            </a>
-        </div>
-        <div class="right-section">
-        </div>
-    </header>
-    <nav class="navigation">
-        <div class="nav-container">
-            <a href=""><img src="../images/Navigation Bar/list-icon.png"></a>
-            <a href="" class="nav-text">List</a>
-        </div>
-        <div class="nav-container">
-            <a href="../html/MainLocation.html"><img src="../images/Navigation Bar/maps-icon.png"></a>
-            <a href="" class="nav-text">Map</a>
+    <nav class="navbar bg-body-tertiary py-5">
+        <div class="container-fluid">
+            <div class="text-center w-100 py-2 text-white">
+                <div class="display-5 fw-semibold d-none d-sm-block">UPDORMINDANAO</div>
+                <div class="display-6 d-none d-md-block">Find Dorms/Boarding House/ Apartment near Mintal</div>
+            </div>
+            <div class="w-100 py-5">
+                <form class="d-flex justify-content-center" role="search">
+                    <input class="form-control me-2 w-50" id="search" name="search" type="search" placeholder="Search" aria-label="Search">
+                    <input type="hidden" name="trans" value = "SEARCH">
+                    <button class="btn btn-danger" type="submit">Search</button>
+                </form>
+            </div>
+            </div>
         </div>
     </nav>
-    <main>
-        <section class="preview-grid">
-            <?php while($row = $result->fetch_assoc()) { ?>
-                <div class="preview-container">
-                    <div class="img-container">
-                        <a href="#" class="btnView" id="<?php echo $row['iddorms'] ?>"><img src=<?php echo $row['imagelink']??""; ?>></a>
-                    </div>
-                    <div class="summary-container">
-                        <div class="first-column">
-                            <a href="#" class="btnView" id="<?php echo $row['iddorms'] ?>"><?php echo $row['name']??""; ?></a>
-                            <p>Bed space: <?php echo $row['roomcapacity']??""; ?></p>
-                            <p>Water: <?php echo $row['water']??""; ?></p>
-                            <p>Electricity: <?php echo $row['electricity']??""; ?></p>
+    <section class="bg-tertiary py-5">
+        <div class="container py-3"> 
+            <div class="row g-5">
+                <?php while($row = $result->fetch_assoc()) { ?>
+                <div class="col-lg-4 col-md-4"> 
+                    <div class="card-custom rounded"> 
+                        <div class="backgroundEffect"></div> 
+                        <div class="pic"> 
+                            <img src=<?php echo $row['imagelink']??""; ?>>
+                        </div> 
+                        <div class="content text-start"> 
+                            <p class="lead mt-4 fw-semibold"><?php echo $row['name']??""; ?></p> 
+                            <p class="text-muted"><div class="h-1">Water: <?php echo $row['water']??""; ?><br>Electricity: <?php echo $row['electricity']??""; ?></div></p> 
+                            <div class="d-inline">
+                            <a href="#" class="btn btn_view" id="<?php echo $row['iddorms'] ?>">View Deal</a>
+                            </div>
                         </div>
                     </div>
                 </div>
-            <?php } ?>
-
-        </section>
-    </main>
-
+                <?php } ?>
+            </div>
+        </div>
+    </section>
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     <script>
 		$(document).ready(function() {
-			$('.btnView').click(function(){	
+			$('.btn_view').click(function(){	
 				var recid = $(this).attr('id');
 				
                 var url = "http://localhost/Dormisko/Dorms/html/detail.php?id=" + recid;
-                window.open(url);
+                window.location.href = url;
 			});
 		});
 	</script>
